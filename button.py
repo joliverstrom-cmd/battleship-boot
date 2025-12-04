7fb24c50ad943a6f9b9fbedbf004ba9bc6d81e81
joliverstrom-cmd <joliverstrom@gmail.com>
2025-12-04 22:51:42 +0100

    Basic placement logic added

--- button.py
+++ button.py
@@ -4,84 +4,33 @@ from functools import partial
 
 class ModeButton(ttk.Button):
     def __init__(self, master, **kwargs):
-        # We extract 'command' from kwargs if it exists, to prevent conflicts, 
-        # though usually we want to force our own command.
 
-        # Initialize the button with our internal check_hit method as the command
-        super().__init__(master, **kwargs, style="primary.TButton")
+        if "style" not in kwargs:
+            kwargs["style"] = "primary.TButton"     
+    
+        super().__init__(master, **kwargs)
 
 class PlacementButton(ttk.Button):
-    def __init__(self, ship_coords, master, **kwargs):
+    def __init__(self, master, row, col, **kwargs):
         
-        super().__init__(master, **kwargs, command = lambda: self.capture_placement(ship_coords))
+        super().__init__(master, **kwargs)
 
-    def capture_placement(self, ship_coords):
-        info = self.grid_info()
-        row_placed = int(info["row"])
-        col_placed = int(info["column"])
-        tup_placed = (row_placed, col_placed)
-        ship_coords.append(tup_placed)
-        print(ship_coords)
-
+        self.row = row
+        self.col = col
 
 class PlayButton(ttk.Button):
-    def __init__(self, target_coords, master, **kwargs):
-        # We extract 'command' from kwargs if it exists, to prevent conflicts, 
-        # though usually we want to force our own command.
-        if 'command' in kwargs:
-            print("Warning: Custom command ignored in favor of check_hit")
-            del kwargs['command']
-
-        # Setting styling so buttons change color when pressed
-        style = ttk.Style()
-
-        #Style for HIT buttons (orange)
-        style.map("Hit.TButton",
-            foreground=[('disabled', 'orange')] 
-        )
-        
-        # Create a style specifically for a MISS (Yellow)
-        style.map("Miss.TButton",
-            foreground=[('disabled', 'yellow')]
-        )
+    def __init__(self, master, row, col, **kwargs):
  
         # Initialize the button with our internal check_hit method as the command
-        super().__init__(master, command= lambda: self.check_hit(target_coords), **kwargs)
+        super().__init__(master, **kwargs)
+        self.row = row
+        self.col = col
   
-    def check_hit(self, target_coords):
+    def set_hit(self):
 
-        strike = False
-        
-        # Get current grid position
-        info = self.grid_info()
-        
-        # specific check to ensure the button has actually been gridded
-        if not info:
-            print("Error: Button has not been placed in a grid yet.")
-            return
-
-        row_hit = int(info["row"])
-        col_hit = int(info["column"])
-        tup_hit = (row_hit, col_hit)
-        
-        if tup_hit in target_coords:
-            print(f"Target hit at {row_hit}, {col_hit}!")
-            strike = True
-        else:
-            print(f"You missed at {row_hit}, {col_hit}")
-
-        self.update_hit_button(strike)
-
-
-    def update_hit_button(self, strike):
-        # Set button to pressed if it hit a target (strike = true)
-        if strike:
-            self.configure(style="Hit.TButton", text =" X ")
-            #self.config(text=" X ")
-        #If it was a miss, set to disabled
-        else:
-            self.configure(style="Miss.TButton", text = " O ")
-            self.config(text="-O-")
-
+        self.configure(style="Hit.TButton", text=" X ")
         self.state(['disabled'])
-        
+    
+    def set_miss(self):
+        self.configure(style="Miss.TButton", text=" O ")
+        self.state(['disabled'])
--- mapboard.py
+++ mapboard.py
@@ -7,32 +7,99 @@ class MapBoard(ttk.Frame):
         super().__init__(master, **kwargs)
         self.rows = rows
         self.cols = cols
+
+        style = ttk.Style()
+        style.map("Hit.TButton", foreground=[('disabled', 'orange')])
+        style.map("Miss.TButton", foreground=[('disabled', 'yellow')])
         
         modebtn = ModeButton(self, text="Start game", command=self.start_game)
-
         modebtn.grid(row=0, column = 0, columnspan=cols*4, sticky="NSEW")
 
+        self.ships = [2,3,4]
         self.ship_coords = []
-
         self.placementbuttons = []
+        self.placedships = []
+
+        for ship in self.ships:
+            i = 0
+            while 0 < i < ship:
+                (previous_x, previous_y) = self.ship_coords[-1]
+                valid_x = [previous_x-1,previous_x+1]
+                valid_y = [previous_y-1,previous_y+1]
+
         
         for r in range(1, self.rows + 1):
             for c in range(1, self.cols + 1):
-                btn = PlacementButton(self.ship_coords, self, text="EMPTY")
+                btn = PlacementButton(self, r, c, text="EMPTY")
                 
-                # The Board decides where the Button goes
+                btn.configure(command = lambda b=btn: self.on_place_click(b))
                 btn.grid(row=r, column=c, sticky="nw") 
                 
                 self.placementbuttons.append(btn)
 
+    def on_place_click(self, btn):
+        
+        coord = (btn.row, btn.col)
+        self.ship_placer(btn, coord)
+
+    #TODO: Create logic to cycle through multiple ships, e.g. by checking how many coordinates have been filled for each ship in self.ships
+    def ship_placer(self, btn, coord ):
+        if len(self.placedships) == 0:
+            self.place_ship_coord(btn, coord)
+            self.placedships.append(coord)
+            return
+        
+        if self.check_valid_place(coord):
+            self.place_ship_coord(btn, coord)
+            self.placedships.append(coord)
+            return
+        else:
+            print("Invalid placement, it has to be adjacent to previous placement")
+
+            
+    def place_ship_coord(self, btn, coord):
+    
+        print(f"Ship placed at {coord}")
+        btn.configure(text="SHIP")
+        self.ship_coords.append(coord)
+            
+    # TODO: Update valid placement logic to cater for straight line
+    def check_valid_place(self, coord):
+        (row, col) = coord
+        (previous_row, previous_col) = self.ship_coords[-1]
+        if previous_row == row+1 or previous_row == row-1:
+            if previous_col == col:
+                return True
+        elif previous_col == col+1 or previous_col == col-1:
+            if previous_row == row:
+                return True
+        else:
+            return False   
+        
+
     def start_game(self):
+        for btn in self.placementbuttons:
+            btn.destroy()
+        
         self.playbuttons = []
         
         for r in range(1, self.rows + 1):
             for c in range(1, self.cols + 1):
-                btn = PlayButton(self.ship_coords, self, text="~s~")
+                btn = PlayButton(self, r, c, text="~s~")
+                
+                btn.configure(command= lambda b=btn: self.on_play_click(b))
                 
                 # The Board decides where the Button goes
                 btn.grid(row=r, column=c, sticky="nw") 
                 
                 self.playbuttons.append(btn)
+
+    def on_play_click(self, btn):
+        coord = (btn.row, btn.col)
+
+        if coord in self.ship_coords:
+            print(f"Hit at {coord}")
+            btn.set_hit()
+        else:
+            print(f"Miss at {coord}")
+            btn.set_miss()
